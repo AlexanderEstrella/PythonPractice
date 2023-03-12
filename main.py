ea9b63e0b074a7e30f277d22f316a0ed91bf669e
AlexanderEstrella <alexanderestrella23@gmail.com>
2023-03-11 19:44:08 -0500

    first python program

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 print("Welcome to the Rapper Name Generator")
 City = input("What's the name of the city you grew up in?\n")
 pet = input("what is the name of your favorite pet?\n")
-print("Your band name could be " + City + " " + pet)
+print("Your Rapper name could be " + City + " " + pet)
