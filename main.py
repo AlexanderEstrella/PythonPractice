307be8cdf75b82718c21ee9036616045aefecbc6
AlexanderEstrella <alexanderestrella23@gmail.com>
2023-03-14 20:41:16 -0400

    data types

--- main.py
+++ main.py
@@ -2,3 +2,20 @@ print("Welcome to the Rapper Name Generator")
 City = input("What's the name of the city you grew up in?\n")
 pet = input("what is the name of your favorite pet?\n")
 print("Your Rapper name could be " + City + " " + pet)
+
+
+# ----------------------------------------
+# Data types
+
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+####################################
+# Write your code below this line 👇
+newdigit = int(two_digit_number[0])
+newdigits = int(two_digit_number[1])
+print(newdigit + newdigits)
+
+# string
+# integer
+# float
+# boolean
